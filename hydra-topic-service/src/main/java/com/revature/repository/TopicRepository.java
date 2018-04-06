1e380c11a831466673637db5e51da5e080ec1400
nknguy <32646451+nknguy@users.noreply.github.com>
2018-04-06 11:11:50 -0400

    added TopicService and TopicRepository

---
+++ hydra-topic-service/src/main/java/com/revature/repository/TopicRepository.java
@@ -0,0 +1,28 @@
+package com.revature.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.revature.model.Topic;
+
+
+@Repository
+public interface TopicRepository extends JpaRepository<Topic, Integer> {
+	
+	/**
+	 * Find Topic by TopicID
+	 * @param TopicId
+	 * @return Topic
+	 */
+	public Topic findOne(Integer id);	
+	/**
+	 * @param batchId
+	 * @return list of Topic for a batch
+	 */
+	public List<Topic> findByBatchId(int batchId);
+	
+	
+	
+}
---
+++ hydra-topic-service/src/main/java/com/revature/services/TopicService.java
@@ -0,0 +1,61 @@
+package com.revature.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.revature.model.Topic;
+import com.revature.repository.TopicRepository;
+
+@Service
+public class TopicService {
+	@Autowired 
+	TopicRepository topicRepo;
+	
+	/**
+	 * This adds a topic to the required topics of a batch
+	 * @param topicName
+	 * @param batchId
+	 * @param weekNumber
+	 */
+	
+	public void addTopic(String topicName, int batchId, int weekNumber) {
+		Topic topic = new Topic();
+		topic.setTopicName(topicName);
+		topic.setBatchID(batchId);
+		topic.setWeekNumber(weekNumber);
+		
+		topicRepo.save(topic);
+	}
+	/**
+	 * Get all topics for a batch
+	 * @param batchId
+	 * @return list of topics for that 1 batch
+	 */
+	public List<Topic> getTopicByBatch(int batchId) {
+		return topicRepo.findByBatchId(batchId);
+	}
+	/**
+	 * Returns all topics in the database
+	 * @return list of topics in the database
+	 */
+	public List<Topic> getTopic(){
+		return topicRepo.findAll();
+	}
+	/**
+	 * This saves or updates Topic object in the database
+	 * @param topic object
+	 */
+	public void addOrUpdateTopicName(Topic topic) {
+		topicRepo.save(topic);
+	}
+	/**
+	 * This returns a Topic object
+	 * @param topicId
+	 * @return a topic
+	 */
+	public Topic getTopicName(int topicId) {
+		return topicRepo.findOne(topicId);
+	}
+}
