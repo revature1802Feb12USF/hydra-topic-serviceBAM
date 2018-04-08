a2422c4b7b26743fe4086298c4d91c78b99d26a0
Jennifer Fox <jrufox@gmail.com>
2018-04-08 15:22:56 -0400

    Swagger and Controller Mapping Changes (Gateway needs to be merged first) (#18) (#19)

--- hydra-topic-service/src/main/java/com/revature/HydraTopicServiceApplication.java
+++ hydra-topic-service/src/main/java/com/revature/HydraTopicServiceApplication.java
@@ -4,6 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @EnableCircuitBreaker
 @EnableEurekaClient
@@ -12,6 +18,10 @@ public class HydraTopicServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HydraTopicServiceApplication.class, args);
 	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.any()).build();
+	}
 }
-
-
--- hydra-topic-service/src/main/java/com/revature/controller/TopicController.java
+++ hydra-topic-service/src/main/java/com/revature/controller/TopicController.java
@@ -3,23 +3,22 @@ package com.revature.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.beans.Topic;
 import com.revature.services.TopicService;
 
 @RestController
-@RequestMapping("/api/v2/Topics/")
 class TopicController {
 
 	@Autowired
-	TopicService topicService;	
-	
+	TopicService topicService;
+
 	/**
 	 * Adds a new TopicName to the TopicName table in the database
 	 * 
-	 * @param name - String for the name of the topic to be added
+	 * @param name
+	 *            - String for the name of the topic to be added
 	 */
 	@PostMapping("/topic")
 	public void addTopic(@RequestBody String name) {
