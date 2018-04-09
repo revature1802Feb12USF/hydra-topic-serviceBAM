61107d935a62b030504231021801136a2e685d95
trevorfortner <trevorfortner1@gmail.com>
2018-04-09 14:06:43 -0400

    Refactored beans to reflect curriculum/batch changes (#20)

--- hydra-topic-service/src/main/java/com/revature/services/TopicService.java
+++ hydra-topic-service/src/main/java/com/revature/services/TopicService.java
@@ -16,18 +16,14 @@ public class TopicService {
 	/**
 	 * This adds a topic to the required topics of a batch
 	 * @param topicName
-	 * @param batchId
-	 * @param weekNumber
 	 */
-	
-	public void addTopic(String topicName, int batchId, int weekNumber) {
+	public void addTopic(String topicName) {
 		Topic topic = new Topic();
 		topic.setTopicName(topicName);
-		topic.setBatchID(batchId);
-		topic.setWeekNumber(weekNumber);
 		
 		topicRepo.save(topic);
 	}
+	
 	/**
 	 * Get all topics for a batch
 	 * @param batchId
@@ -36,6 +32,7 @@ public class TopicService {
 	public List<Topic> getTopicByBatch(int batchId) {
 		return topicRepo.findByBatchID(batchId);
 	}
+	
 	/**
 	 * Returns all topics in the database
 	 * @return list of topics in the database
@@ -43,6 +40,7 @@ public class TopicService {
 	public List<Topic> getTopic(){
 		return topicRepo.findAll();
 	}
+	
 	/**
 	 * This saves or updates Topic object in the database
 	 * @param topic object
@@ -50,6 +48,7 @@ public class TopicService {
 	public void addOrUpdateTopic(Topic topic) {
 		topicRepo.save(topic);
 	}
+	
 	/**
 	 * This returns a Topic object
 	 * @param topicId
