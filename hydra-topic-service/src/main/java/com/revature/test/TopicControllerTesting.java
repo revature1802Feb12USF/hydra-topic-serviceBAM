83da310ba7d83c9238e08f9b706036f0ae09c137
ps763 <36372182+ps763@users.noreply.github.com>
2018-04-10 22:06:52 -0400

    testing for topic complete

--- hydra-topic-service/src/main/java/com/revature/test/TopicControllerTesting.java
+++ hydra-topic-service/src/main/java/com/revature/test/TopicControllerTesting.java
@@ -1,14 +1,16 @@
 package com.revature.test;
 
+import static io.restassured.RestAssured.*;
+import static io.restassured.RestAssured.when;
+
+import java.util.Map;
+
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.revature.beans.Topic;
 
-import io.restassured.RestAssured;
-
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
+import io.restassured.http.ContentType;
 
 
 public class TopicControllerTesting {
@@ -22,11 +24,18 @@ public class TopicControllerTesting {
 	 */
 	@Test
 	public void addTopicTest() {
-		
+
+		given().
+		body("{\"id\":\"2\", "
+				+ "\"name\"  ").
+		when().
+		contentType(ContentType.JSON).
+		post("http://localhost:8080/topic");
+/*
 		when().
 	       post("http://localhost:8080/topic").then().assertThat().
 	       statusCode(200).
-	       extract().response();
+	       extract().response();	*/
 	}
 
 	/**
@@ -37,7 +46,7 @@ public class TopicControllerTesting {
 	public void getAllTopicsTest() {
 		
 	    when().
-	       get("http://localhost:8080/topics").then().assertThat().
+	       get("http://localhost:8080/").then().assertThat().
 	       statusCode(200).
 	       extract().response();
 	}
